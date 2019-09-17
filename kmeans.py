fd4a5757c5419c1a85145cbde133f460d0685c6e
MChoir24 <miftahulkhoir9@gmail.com>
2019-09-17 17:36:57 +0700

    kmeans error ln 28

--- kmeans.py
+++ kmeans.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 
 def ecludian(x, y):
-    tempx = (x[0] - x[1]) ** 2
-    tempy = (y[0] - y[1]) ** 2
+    tempx = (x[0] - y[0]) ** 2
+    tempy = (x[1] - y[1]) ** 2
     result = (tempx + tempy) ** 0.5
     return result
 
@@ -18,17 +18,22 @@ def kmean(data, center, n):
             temp0 = ecludian(data[i], center[0])
             temp1 = ecludian(data[i], center[1])
             label[i] = np.argmin([temp0, temp1])
-            print(i, temp0, temp1)
+            # print(i, temp0, temp1)
             if label[i] == 1:
                 clus1.append(data[i])
             else:
                 clus0.append(data[i])
+            clus0 = np.array(clus0)
+            clus1 = np.array(clus1)
+            plt.plot(clus0[:, 0], clus0[:, 1], 'ro', clus1[:, 0], clus1[:, 1], 'ro')
+            plt.show()
         center[0] = np.average(np.array(clus0), axis=0)
         center[1] = np.average(np.array(clus1), axis=0)
         print(center)
+    return center
 
 
 data = np.array([[7,5],[5,7],[7,7],[3,3],[4,6],[1,4],[0,0],[2,2],[8,7],[6,8]])
 center = np.array([[4,6],[5,5]], dtype=float)
 
-print(kmean(data, center, 1))
+print(kmean(data, center, 10))
